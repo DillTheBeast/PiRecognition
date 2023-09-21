acdf7cc7760a7a0582e1329d1ab4926e9859a16e
DillTheBeast <dillonmaltese@gmail.com>
2023-09-21 13:24:07 -0400

    as

--- recognition.py
+++ recognition.py
@@ -1,10 +1,21 @@
 import cv2
 import time
+import face_recognition
 
 img = cv2.imread("Messi1.webp")
+#Switching color type
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+img2 = cv2.imread("Elon Musk.jpg")
+#Switching color type
+rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
 cv2.imshow("IMG", img)
+cv2.imshow("IMG", img2)
 cv2.waitKey(0)
 
 time.sleep(1)
+cv2.destroyAllWindows()
 quit()
